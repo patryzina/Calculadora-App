20e7d2aae606ec796dbd25b003ba7f6cb454cb80
patryzina <patryzina@gmail.com>
2020-04-15 16:25:14 +0200

    se modifica principalmente el HTML de la calculadora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,11 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CalculadoraComponent } from './calculadora/calculadora.component';
-import { OperandoComponent } from './operando/operando.component';
-import { SumaComponent } from './suma/suma.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CalculadoraComponent,
-    OperandoComponent,
-    SumaComponent
-
+    AppComponent
   ],
   imports: [
     BrowserModule,
